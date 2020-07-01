31d078e8699bc1479613bd466e925b690252a156
Rati1324 <61045363+Rati1324@users.noreply.github.com>
2020-07-01 17:37:10 +0400

    Update settings.py

--- message_board/settings.py
+++ message_board/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'account.apps.AccountConfig',
     'homepage.apps.HomepageConfig',
-    "crispy_forms",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
